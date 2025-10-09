5ae3db5e5367bcdad8ddcfe1df7c3945e335a54e
Mara <marastodulka@gmail.com>
2025-10-09 21:34:49 +0200

    daily commit part 67

--- main.lua
+++ main.lua
@@ -972,6 +972,7 @@ function processSplashDamage(proj, targetEnemy, hasSlow)
 end
 
 --TODO:
+-- info box for tower stats on their left side
 -- Balancing game difficulty and economy
 -- Add sound effects and music
 -- More maps and level selection menu
