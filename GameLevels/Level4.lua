64a37a6597ef0e2326b2ad8fe3cf24deb645368f
Mark <marastodulka@gmail.com>
2025-10-14 15:16:56 +0200

    Lvl 4

---
+++ GameLevels/Level4.lua
@@ -0,0 +1,250 @@
+Level4 = {
+    wave1 = {
+        enemies = {
+            {type = "zombie", count = 8},
+            {type = "fastZombie", count = 4}
+        },
+        spawnRate = 0.6,
+        reward = 100
+    },
+    wave2 = {
+        enemies = {
+            {type = "zombie", count = 10},
+            {type = "tank", count = 2}
+        },
+        spawnRate = 0.55,
+        reward = 130
+    },
+    wave3 = {
+        enemies = {
+            {type = "fastZombie", count = 12},
+            {type = "zombie", count = 6}
+        },
+        spawnRate = 0.5,
+        reward = 160
+    },
+    wave4 = {
+        enemies = {
+            {type = "tank", count = 4},
+            {type = "zombie", count = 10}
+        },
+        spawnRate = 0.55,
+        reward = 180
+    },
+    wave5 = {
+        enemies = {
+            {type = "fastZombie", count = 10},
+            {type = "tank", count = 4},
+            {type = "hidden", count = 3}
+        },
+        spawnRate = 0.5,
+        reward = 220
+    },
+    wave6 = {
+        enemies = {
+            {type = "tank", count = 6},
+            {type = "zombie", count = 10}
+        },
+        spawnRate = 0.45,
+        reward = 250
+    },
+    wave7 = {
+        enemies = {
+            {type = "fastZombie", count = 15},
+            {type = "tank", count = 4},
+            {type = "hidden", count = 2}
+        },
+        spawnRate = 0.5,
+        reward = 280
+    },
+    wave8 = {
+        enemies = {
+            {type = "Normalboss", count = 1}
+        },
+        spawnRate = 1.2,
+        reward = 400
+    },
+    wave9 = {
+        enemies = {
+            {type = "zombie", count = 15},
+            {type = "tank", count = 6}
+        },
+        spawnRate = 0.5,
+        reward = 320
+    },
+    wave10 = {
+        enemies = {
+            {type = "fastZombie", count = 12},
+            {type = "Tankboss", count = 1}
+        },
+        spawnRate = 0.8,
+        reward = 600
+    },
+    wave11 = {
+        enemies = {
+            {type = "tank", count = 8},
+            {type = "zombie", count = 12}
+        },
+        spawnRate = 0.45,
+        reward = 350
+    },
+    wave12 = {
+        enemies = {
+            {type = "tank", count = 10},
+            {type = "hidden", count = 3}
+        },
+        spawnRate = 0.5,
+        reward = 380
+    },
+    wave13 = {
+        enemies = {
+            {type = "fastZombie", count = 18},
+            {type = "zombie", count = 10}
+        },
+        spawnRate = 0.45,
+        reward = 420
+    },
+    wave14 = {
+        enemies = {
+            {type = "tank", count = 12},
+            {type = "fastZombie", count = 6}
+        },
+        spawnRate = 0.45,
+        reward = 460
+    },
+    wave15 = {
+        enemies = {
+            {type = "Fastboss", count = 1},
+            {type = "zombie", count = 10}
+        },
+        spawnRate = 0.9,
+        reward = 700
+    },
+    wave16 = {
+        enemies = {
+            {type = "tank", count = 10},
+            {type = "zombie", count = 12},
+            {type = "hidden", count = 4}
+        },
+        spawnRate = 0.45,
+        reward = 480
+    },
+    wave17 = {
+        enemies = {
+            {type = "fastZombie", count = 20},
+            {type = "tank", count = 8}
+        },
+        spawnRate = 0.4,
+        reward = 500
+    },
+    wave18 = {
+        enemies = {
+            {type = "Tankboss", count = 1},
+            {type = "tank", count = 8}
+        },
+        spawnRate = 0.8,
+        reward = 800
+    },
+    wave19 = {
+        enemies = {
+            {type = "zombie", count = 20},
+            {type = "fastZombie", count = 10}
+        },
+        spawnRate = 0.4,
+        reward = 520
+    },
+    wave20 = {
+        enemies = {
+            {type = "Gravedigger", count = 1},
+            {type = "tank", count = 5}
+        },
+        spawnRate = 1,
+        reward = 1000
+    },
+    wave21 = {
+        enemies = {
+            {type = "fastZombie", count = 20},
+            {type = "tank", count = 10},
+            {type = "zombie", count = 8}
+        },
+        spawnRate = 0.4,
+        reward = 600
+    },
+    wave22 = {
+        enemies = {
+            {type = "tank", count = 15},
+            {type = "zombie", count = 12}
+        },
+        spawnRate = 0.35,
+        reward = 650
+    },
+    wave23 = {
+        enemies = {
+            {type = "Destroyer", count = 1},
+            {type = "tank", count = 6},
+            {type = "fastZombie", count = 8}
+        },
+        spawnRate = 0.7,
+        reward = 900
+    },
+    wave24 = {
+        enemies = {
+            {type = "tank", count = 20},
+            {type = "zombie", count = 10}
+        },
+        spawnRate = 0.35,
+        reward = 700
+    },
+    wave25 = {
+        enemies = {
+            {type = "Ultimateboss", count = 1},
+            {type = "tank", count = 5}
+        },
+        spawnRate = 1.2,
+        reward = 1200
+    },
+    wave26 = {
+        enemies = {
+            {type = "fastZombie", count = 25},
+            {type = "tank", count = 10}
+        },
+        spawnRate = 0.4,
+        reward = 800
+    },
+    wave27 = {
+        enemies = {
+            {type = "ScreenMuncher", count = 1},
+            {type = "tank", count = 12},
+            {type = "zombie", count = 10}
+        },
+        spawnRate = 1,
+        reward = 1500
+    },
+    wave28 = {
+        enemies = {
+            {type = "Fastboss", count = 1},
+            {type = "Tankboss", count = 1}
+        },
+        spawnRate = 1.5,
+        reward = 2000
+    },
+    wave29 = {
+        enemies = {
+            {type = "Gravedigger", count = 1},
+            {type = "Ultimateboss", count = 1}
+        },
+        spawnRate = 1.5,
+        reward = 2500
+    },
+    wave30 = {
+        enemies = {
+            {type = "TheONE", count = 1},
+            {type = "Destroyer", count = 1},
+            {type = "ScreenMuncher", count = 1}
+        },
+        spawnRate = 3,
+        reward = 0
+    }
+}
+
+return Level4
