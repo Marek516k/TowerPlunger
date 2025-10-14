cfad64ceeb1f351cbddfdc6b9c93ca914f7bbad9
Mark <marastodulka@gmail.com>
2025-10-14 20:06:39 +0200

    Lvl 6

--- GameLevels/Level6.lua
+++ GameLevels/Level6.lua
@@ -1,4 +1,177 @@
-Map2 = {
+Level6 = {
+    -- Early build-up
+    wave1 = {
+        enemies = {
+            {type = "zombie", count = 8},
+            {type = "skeleton", count = 6}
+        },
+        spawnRate = 0.6,
+        reward = 150
+    },
+    wave2 = {
+        enemies = {
+            {type = "fastZombie", count = 8},
+            {type = "hidden", count = 4}
+        },
+        spawnRate = 0.55,
+        reward = 170
+    },
+    wave3 = {
+        enemies = {
+            {type = "skeleton", count = 10},
+            {type = "hidden", count = 6}
+        },
+        spawnRate = 0.5,
+        reward = 200
+    },
+    wave4 = {
+        enemies = {
+            {type = "tank", count = 4},
+            {type = "boneReaper", count = 2}
+        },
+        spawnRate = 0.5,
+        reward = 230
+    },
+    wave5 = {
+        enemies = {
+            {type = "armoredSkeleton", count = 4},
+            {type = "hidden", count = 8}
+        },
+        spawnRate = 0.45,
+        reward = 260
+    },
+
+    -- Mid progression
+    wave6 = {
+        enemies = {
+            {type = "fastZombie", count = 10},
+            {type = "boneReaper", count = 3}
+        },
+        spawnRate = 0.45,
+        reward = 300
+    },
+    wave7 = {
+        enemies = {
+            {type = "tank", count = 6},
+            {type = "hidden", count = 10}
+        },
+        spawnRate = 0.4,
+        reward = 330
+    },
+    wave8 = {
+        enemies = {
+            {type = "armoredSkeleton", count = 6},
+            {type = "hidden", count = 6}
+        },
+        spawnRate = 0.4,
+        reward = 360
+    },
+    wave9 = {
+        enemies = {
+            {type = "Hiddenboss", count = 1},
+            {type = "tank", count = 4}
+        },
+        spawnRate = 1.0,
+        reward = 500
+    },
+    wave10 = {
+        enemies = {
+            {type = "boneReaper", count = 5},
+            {type = "hidden", count = 10}
+        },
+        spawnRate = 0.4,
+        reward = 420
+    },
+
+    -- Rising threat
+    wave11 = {
+        enemies = {
+            {type = "tank", count = 8},
+            {type = "armoredSkeleton", count = 6}
+        },
+        spawnRate = 0.4,
+        reward = 460
+    },
+    wave12 = {
+        enemies = {
+            {type = "Hiddenboss", count = 1},
+            {type = "boneReaper", count = 4}
+        },
+        spawnRate = 0.9,
+        reward = 600
+    },
+    wave13 = {
+        enemies = {
+            {type = "tank", count = 10},
+            {type = "hidden", count = 10}
+        },
+        spawnRate = 0.35,
+        reward = 650
+    },
+    wave14 = {
+        enemies = {
+            {type = "Fastboss", count = 1},
+            {type = "armoredSkeleton", count = 5}
+        },
+        spawnRate = 1.0,
+        reward = 900
+    },
+    wave15 = {
+        enemies = {
+            {type = "hiddenBrainrot", count = 1},
+            {type = "boneReaper", count = 4}
+        },
+        spawnRate = 0.8,
+        reward = 1000
+    },
+
+    -- Final stretch
+    wave16 = {
+        enemies = {
+            {type = "Tankboss", count = 1},
+            {type = "hidden", count = 10}
+        },
+        spawnRate = 1.2,
+        reward = 1200
+    },
+    wave17 = {
+        enemies = {
+            {type = "GhostPhantasm", count = 1},
+            {type = "armoredSkeleton", count = 6}
+        },
+        spawnRate = 1.3,
+        reward = 1400
+    },
+    wave18 = {
+        enemies = {
+            {type = "Ultimateboss", count = 1},
+            {type = "boneReaper", count = 5}
+        },
+        spawnRate = 1.5,
+        reward = 1700
+    },
+    wave19 = {
+        enemies = {
+            {type = "hiddenBrainrot", count = 2},
+            {type = "tank", count = 8}
+        },
+        spawnRate = 0.8,
+        reward = 2000
+    },
+
+    -- 💀 Final Boss Wave
+    wave20 = {
+        enemies = {
+            {type = "Destroyer", count = 1},
+            {type = "Hiddenboss", count = 1},
+            {type = "boneReaper", count = 3}
+        },
+        spawnRate = 2.5,
+        reward = 2500
+    }
+}
+
+Map = {
     "#################################",
     "........................#########",
     "#######################.#########",
@@ -21,3 +194,5 @@ Map2 = {
     "#################################",
     "#################################"
 }
+
+return Level6, Map
