c01cc4d5af0ee1785126547995450270787d502b
Mark <marastodulka@gmail.com>
2025-10-14 15:30:24 +0200

    Lvl 5

---
+++ GameLevels/Level5.lua
@@ -0,0 +1,286 @@
+Level5 = {
+    wave1 = {
+        enemies = {
+            {type = "zombie", count = 6},
+            {type = "skeleton", count = 4}
+        },
+        spawnRate = 0.6,
+        reward = 100
+    },
+    wave2 = {
+        enemies = {
+            {type = "zombie", count = 8},
+            {type = "hidden", count = 4}
+        },
+        spawnRate = 0.55,
+        reward = 120
+    },
+    wave3 = {
+        enemies = {
+            {type = "skeleton", count = 10},
+            {type = "fastZombie", count = 4}
+        },
+        spawnRate = 0.5,
+        reward = 150
+    },
+    wave4 = {
+        enemies = {
+            {type = "hidden", count = 8},
+            {type = "tank", count = 3}
+        },
+        spawnRate = 0.55,
+        reward = 180
+    },
+    wave5 = {
+        enemies = {
+            {type = "skeleton", count = 8},
+            {type = "hidden", count = 8}
+        },
+        spawnRate = 0.5,
+        reward = 200
+    },
+    wave6 = {
+        enemies = {
+            {type = "zombie", count = 10},
+            {type = "hidden", count = 10}
+        },
+        spawnRate = 0.45,
+        reward = 230
+    },
+    wave7 = {
+        enemies = {
+            {type = "fastZombie", count = 10},
+            {type = "boneReaper", count = 2}
+        },
+        spawnRate = 0.5,
+        reward = 260
+    },
+    wave8 = {
+        enemies = {
+            {type = "armoredSkeleton", count = 4},
+            {type = "hidden", count = 8}
+        },
+        spawnRate = 0.5,
+        reward = 300
+    },
+    wave9 = {
+        enemies = {
+            {type = "tank", count = 5},
+            {type = "hidden", count = 10}
+        },
+        spawnRate = 0.5,
+        reward = 320
+    },
+    wave10 = {
+        enemies = {
+            {type = "Normalboss", count = 1},
+            {type = "hidden", count = 6}
+        },
+        spawnRate = 1.2,
+        reward = 500
+    },
+    wave11 = {
+        enemies = {
+            {type = "boneReaper", count = 3},
+            {type = "fastZombie", count = 10}
+        },
+        spawnRate = 0.45,
+        reward = 350
+    },
+    wave12 = {
+        enemies = {
+            {type = "armoredSkeleton", count = 6},
+            {type = "hidden", count = 6}
+        },
+        spawnRate = 0.45,
+        reward = 380
+    },
+    wave13 = {
+        enemies = {
+            {type = "tank", count = 8},
+            {type = "hidden", count = 8}
+        },
+        spawnRate = 0.4,
+        reward = 420
+    },
+    wave14 = {
+        enemies = {
+            {type = "hidden", count = 12},
+            {type = "skeleton", count = 10}
+        },
+        spawnRate = 0.4,
+        reward = 460
+    },
+    wave15 = {
+        enemies = {
+            {type = "Hiddenboss", count = 1},
+            {type = "armoredSkeleton", count = 4}
+        },
+        spawnRate = 0.9,
+        reward = 700
+    },
+    wave16 = {
+        enemies = {
+            {type = "tank", count = 10},
+            {type = "hidden", count = 10}
+        },
+        spawnRate = 0.4,
+        reward = 480
+    },
+    wave17 = {
+        enemies = {
+            {type = "fastZombie", count = 15},
+            {type = "boneReaper", count = 4}
+        },
+        spawnRate = 0.4,
+        reward = 500
+    },
+    wave18 = {
+        enemies = {
+            {type = "Hiddenboss", count = 1},
+            {type = "hiddenBrainrot", count = 1}
+        },
+        spawnRate = 0.9,
+        reward = 850
+    },
+    wave19 = {
+        enemies = {
+            {type = "armoredSkeleton", count = 8},
+            {type = "hidden", count = 8}
+        },
+        spawnRate = 0.4,
+        reward = 520
+    },
+    wave20 = {
+        enemies = {
+            {type = "Tankboss", count = 1},
+            {type = "hidden", count = 6}
+        },
+        spawnRate = 1.0,
+        reward = 1000
+    },
+    wave21 = {
+        enemies = {
+            {type = "boneReaper", count = 6},
+            {type = "tank", count = 8}
+        },
+        spawnRate = 0.4,
+        reward = 600
+    },
+    wave22 = {
+        enemies = {
+            {type = "hidden", count = 14},
+            {type = "armoredSkeleton", count = 7}
+        },
+        spawnRate = 0.35,
+        reward = 650
+    },
+    wave23 = {
+        enemies = {
+            {type = "Destroyer", count = 1},
+            {type = "hidden", count = 10}
+        },
+        spawnRate = 0.8,
+        reward = 900
+    },
+    wave24 = {
+        enemies = {
+            {type = "tank", count = 10},
+            {type = "hidden", count = 10}
+        },
+        spawnRate = 0.35,
+        reward = 700
+    },
+    wave25 = {
+        enemies = {
+            {type = "Ultimateboss", count = 1},
+            {type = "boneReaper", count = 5}
+        },
+        spawnRate = 1.0,
+        reward = 1200
+    },
+    wave26 = {
+        enemies = {
+            {type = "fastZombie", count = 25},
+            {type = "hidden", count = 12}
+        },
+        spawnRate = 0.4,
+        reward = 800
+    },
+    wave27 = {
+        enemies = {
+            {type = "GhostPhantasm", count = 1},
+            {type = "hidden", count = 12}
+        },
+        spawnRate = 1.0,
+        reward = 1500
+    },
+    wave28 = {
+        enemies = {
+            {type = "Tankboss", count = 1},
+            {type = "Hiddenboss", count = 1}
+        },
+        spawnRate = 1.3,
+        reward = 1800
+    },
+    wave29 = {
+        enemies = {
+            {type = "armoredSkeleton", count = 10},
+            {type = "hiddenBrainrot", count = 1}
+        },
+        spawnRate = 0.8,
+        reward = 2000
+    },
+    wave30 = {
+        enemies = {
+            {type = "ScreenMuncher", count = 1},
+            {type = "Destroyer", count = 1}
+        },
+        spawnRate = 1.8,
+        reward = 2500
+    },
+    wave31 = {
+        enemies = {
+            {type = "hidden", count = 20},
+            {type = "tank", count = 10}
+        },
+        spawnRate = 0.4,
+        reward = 1000
+    },
+    wave32 = {
+        enemies = {
+            {type = "Fastboss", count = 1},
+            {type = "boneReaper", count = 6}
+        },
+        spawnRate = 1.0,
+        reward = 2200
+    },
+    wave33 = {
+        enemies = {
+            {type = "GhostPhantasm", count = 1},
+            {type = "Ultimateboss", count = 1}
+        },
+        spawnRate = 2.0,
+        reward = 3000
+    },
+    wave34 = {
+        enemies = {
+            {type = "Destroyer", count = 1},
+            {type = "Hiddenboss", count = 1},
+            {type = "boneReaper", count = 5}
+        },
+        spawnRate = 1.5,
+        reward = 3500
+    },
+    wave35 = {
+        enemies = {
+            {type = "TheONE", count = 1},
+            {type = "GhostPhantasm", count = 1},
+            {type = "hiddenBrainrot", count = 1}
+        },
+        spawnRate = 3.0,
+        reward = 0
+    }
+}
+
+return Level5
